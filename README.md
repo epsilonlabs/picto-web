1241859ec26e5cde7c86191b948050066d542c1c
alfa-yohannis <ary506@york.ac.uk>
2022-07-21 21:48:25 +0100

    Update README
    Update README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# picto-web
-The Web Version of Picto
+# [Picto Web](#contents)
+
+## [Contents](#contents)
+- [Picto Web](#picto-web)
+  - [Contents](#contents)
+  - [Introduction](#introduction)
+  - [Docker](#docker)
+  - [Demo](#demo)
+
+## [Introduction](#contents)
+[Picto Web](https://github.com/epsilonlabs/picto-web) is a tool for complex model exploration. With it, users can visualise large models in different views and formats and also to perform drill down/up navigation throughtout the models' elements. Picto Web is a the web version of the [Picto](https://www.eclipse.org/epsilon/doc/picto/) for Eclipse. It aims to supports all features that the plugin capable of except that it is a web application, which allows multiple, remote users to explore a same model concurrently. 
+
+The app is able to read *.picto files and display the view trees and views of the files. It is also able to monitor files in a directory if they have changed and send the updates to the front-end for  auto-refresh. Picto Web also refactors [Picto](https://www.eclipse.org/epsilon/doc/picto/) so that its core libaries is independent of the Eclipse platform. The app is packaged in a [Docker container](https://hub.docker.com/r/alfayohannisyorkacuk/picto-web).
+
+## [Docker](#contents)
+The docker image of Picto Web can be temporarily found here: [https://hub.docker.com/r/alfayohannisyorkacuk/picto-web](https://hub.docker.com/r/alfayohannisyorkacuk/picto-web). Execute the following command to pull it from https://hub.docker.com:
+```
+docker pull alfayohannisyorkacuk/picto-web
+```
+If your operating system is Windows, run the command below to run the Picto Web server. The variable `%cd%` represents your current working directory in Windows. 
+```
+docker run --rm -i -t -v %cd%:/workspace --hostname=picto -p 8080:8080 --name=picto alfayohannisyorkacuk/picto-web
+```
+Replace `%cd%` with `$PWD`, if you use Linux as your operating system, 
+```
+docker run --rm -i -t -v $PWD:/workspace --hostname=picto -p 8080:8080 --name=picto alfayohannisyorkacuk/picto-web
+```
+or replace it with any directory path if you want to set the directory as the working directory of Picto Web.
+
+## [Demo](#contents)
+Browse http://localhost:8080 to load Picto Web app.
--- org.eclipse.epsilon.picto.web/src/test/java/org/eclipse/epsilon/picto/incrementality/IncrementalityTest.java
+++ org.eclipse.epsilon.picto.web/src/test/java/org/eclipse/epsilon/picto/incrementality/IncrementalityTest.java
@@ -70,203 +70,203 @@ class IncrementalityTest {
 	void tearDown() throws Exception {
 	}
 
-	@SuppressWarnings("unchecked")
-	@Test
-	void testOptimisation() throws Exception {
-		try {
-			File metamodelFile = new File(PictoApplication.WORKSPACE + "socialnetwork.ecore");
-			File modelFile = new File(PictoApplication.WORKSPACE + "socialnetwork.model");
-			File egxFile = new File(PictoApplication.WORKSPACE + "picto" + File.separator + "socialnetwork.egx");
-
-			// register metamodel
-			org.eclipse.emf.common.util.URI uri = org.eclipse.emf.common.util.URI
-					.createFileURI(metamodelFile.getAbsolutePath());
-			EmfUtil.register(uri, EPackage.Registry.INSTANCE);
-
-			// load model
-			ResourceSet resourceSet = new ResourceSetImpl();
-			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("model",
-					new XMIResourceFactoryImpl());
-			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi",
-					new XMIResourceFactoryImpl());
-			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new FlexmiResourceFactory());
-			Resource resource = resourceSet
-					.getResource(org.eclipse.emf.common.util.URI.createFileURI(modelFile.getAbsolutePath()), true);
-			resource.load(null);
-
-			IModel model = new InMemoryEmfModel("M", resource);
-			((InMemoryEmfModel) model).setExpand(true);
-
-			// load transformation
-			IncrementalResource incrementalResource = new PropertyAccessRecordTable();
-			IncrementalLazyEgxModule module = new IncrementalLazyEgxModule(incrementalResource);
-			IEolContext context = module.getContext();
-			URI transformationUri = egxFile.toURI();
-
-			module.parse(transformationUri);
-
-			// add model to context
-			context.getModelRepository().addModel(model);
-
-			/** execute transformation after model is changed **/
-			List<LazyGenerationRuleContentPromise> instances = (List<LazyGenerationRuleContentPromise>) module.execute();
-			((IncrementalLazyEgxModule) module).startRecording();
-			generateViews(instances, module);
-			((IncrementalLazyEgxModule) module).stopRecording();
-
-			((IncrementalLazyEgxModule) module).getPropertyAccessRecorder().getPropertyAccesses().clear();
-			
-			incrementalResource.printIncrementalRecords();
-
-			// modify the model
-			EObject eObject = (EObject) model.getElementById("alice");
-			EStructuralFeature eNameFeature = eObject.eClass().getEStructuralFeature("name");
-			System.out.println("Element's name: " + (String) eObject.eGet(eNameFeature));
-			eObject.eSet(eNameFeature, "Amber");
-
-			/** reexecute transformation after model is changed **/
-			instances = (List<LazyGenerationRuleContentPromise>) module.execute();
-			((IncrementalLazyEgxModule) module).startRecording();
-			generateViews(instances, module);
-			((IncrementalLazyEgxModule) module).stopRecording();
-
-			incrementalResource.printIncrementalRecords();
-
-			System.out.println("Targets of Modified Properties:" + incrementalResource.getModifiedPropertiesTargets().size());
-			
-			for (PropertyAccessRecord property : incrementalResource.getModifiedPropertiesTargets()) {
-				System.out.println(property);
-			}
-			assertEquals(incrementalResource.getModifiedPropertiesTargets().size(), 2);
-			System.out.println("Finished!");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void generateViews(List<LazyGenerationRuleContentPromise> instances, IncrementalLazyEgxModule module) {
-		for (LazyGenerationRuleContentPromise instance : instances) {
-			
-			String format = "svg";
-			String icon = "";
-			List<Patch> patches = new ArrayList<>(1);
-			Collection<String> path = Arrays.asList("");
-			String pathString = null;
-			List<Layer> layers = new ArrayList<>();
-			Variable layersVariable = null;
-			Integer position = null;
-
-			Collection<Variable> instanceVariables = instance.getVariables();
-
-			for (Variable variable : instanceVariables) {
-				Object varValue = variable.getValue();
-				switch (variable.getName()) {
-				case "format": {
-					format = varValue + "";
-					break;
-				}
-				case "path": {
-					if (!(varValue instanceof Collection)) {
-						(path = (Collection<String>) (varValue = new ArrayList<>(1))).add(Objects.toString(varValue));
-					} else if (!((Collection<?>) varValue).isEmpty()) {
-						path = ((Collection<?>) varValue).stream().map(Objects::toString).collect(Collectors.toList());
-					}
-					pathString = "/" + String.join("/", path);
-					break;
-				}
-				case "icon": {
-					icon = varValue + "";
-					break;
-				}
-				case "position": {
-					if (varValue instanceof Integer) {
-						position = (Integer) varValue;
-					} else if (varValue != null) {
-						position = Integer.parseInt(varValue.toString());
-					}
-					break;
-				}
-				case "layers": {
-					layersVariable = variable;
-					for (Object layerMapObject : (Iterable<?>) varValue) {
-						Map<Object, Object> layerMap = (Map<Object, Object>) layerMapObject;
-						Layer layer = new Layer();
-						layer.setId(layerMap.get("id") + "");
-						layer.setTitle(layerMap.get("title") + "");
-						if (layerMap.containsKey("active")) {
-							layer.setActive((boolean) layerMap.get("active"));
-						}
-						layers.add(layer);
-					}
-					break;
-				}
-				case "patches": {
-					if (varValue instanceof List) {
-						patches = (List<Patch>) varValue;
-					} else if (varValue instanceof Patch) {
-						patches.add((Patch) varValue);
-					} else if (varValue instanceof Collection) {
-						patches.addAll((Collection<? extends Patch>) varValue);
-					}
-					break;
-				}
-				}
-
-			}
-
-			// If this is a custom view there may be an activeLayers variable in the
-			// variables list
-			Variable activeLayersVariable = instanceVariables.stream().filter(v -> v.getName().equals("activeLayers"))
-					.findAny().orElse(null);
-			if (activeLayersVariable != null) {
-				Collection<?> activeLayers = (Collection<?>) activeLayersVariable.getValue();
-				for (Layer layer : layers) {
-					layer.setActive(activeLayers.contains(layer.getId()));
-				}
-			}
-
-			// Replace layers variable from list of maps to list of Layer objects
-			if (layersVariable != null) {
-				instanceVariables.remove(layersVariable);
-			}
-			instanceVariables.add(Variable.createReadOnlyVariable("layers", layers));
-
-			ViewTree vt = new ViewTree(instance, format, icon, position, patches, layers);
-			vt.getContent();
-			updateIncrementalResource(module, pathString);
-
-		}
-	}
-	
-	private void updateIncrementalResource(IncrementalLazyEgxModule module, String pathString) {
-		for (IPropertyAccess propertyAccess : ((IncrementalLazyEgxModule) module).getPropertyAccessRecorder()
-				.getPropertyAccesses().all()) {
-			GenerationRulePropertyAccess generationRulePropertyAccess = (GenerationRulePropertyAccess) propertyAccess;
-
-			EObject contextElement = (EObject) generationRulePropertyAccess.getContextElement();
-			String contextResourceUri = null;
-			String contextElementId = null;
-			if (contextElement != null) {
-				contextResourceUri = ((XMIResource) contextElement.eResource()).getURI().toString();
-				contextElementId = ((XMIResource) contextElement.eResource()).getID(contextElement);
-			} else {
-				System.console();
-			}
-
-			EObject modelElement = (EObject) generationRulePropertyAccess.getModelElement();
-			String elementResourceUri = ((XMIResource) modelElement.eResource()).getURI().toString();
-			String elementId = ((XMIResource) modelElement.eResource()).getID(modelElement);
-
-			String propertyName = generationRulePropertyAccess.getPropertyName();
-			Object value = modelElement.eGet(modelElement.eClass().getEStructuralFeature(propertyName));
-
-			PropertyAccessRecord record = new PropertyAccessRecord(module.getFile().getAbsolutePath(),
-					generationRulePropertyAccess.getRule().getName(), contextResourceUri, contextElementId,
-					elementResourceUri, elementId, propertyName, value, pathString);
-			System.out.println("added: " + record);
-			module.getIncrementalResource().add(record);
-		}
-		((IncrementalLazyEgxModule) module).getPropertyAccessRecorder().getPropertyAccesses().clear();
-		System.console();
-	}
+//	@SuppressWarnings("unchecked")
+//	@Test
+//	void testOptimisation() throws Exception {
+////		try {
+////			File metamodelFile = new File(PictoApplication.WORKSPACE + "socialnetwork.ecore");
+////			File modelFile = new File(PictoApplication.WORKSPACE + "socialnetwork.model");
+////			File egxFile = new File(PictoApplication.WORKSPACE + "picto" + File.separator + "socialnetwork.egx");
+////
+////			// register metamodel
+////			org.eclipse.emf.common.util.URI uri = org.eclipse.emf.common.util.URI
+////					.createFileURI(metamodelFile.getAbsolutePath());
+////			EmfUtil.register(uri, EPackage.Registry.INSTANCE);
+////
+////			// load model
+////			ResourceSet resourceSet = new ResourceSetImpl();
+////			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("model",
+////					new XMIResourceFactoryImpl());
+////			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi",
+////					new XMIResourceFactoryImpl());
+////			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new FlexmiResourceFactory());
+////			Resource resource = resourceSet
+////					.getResource(org.eclipse.emf.common.util.URI.createFileURI(modelFile.getAbsolutePath()), true);
+////			resource.load(null);
+////
+////			IModel model = new InMemoryEmfModel("M", resource);
+////			((InMemoryEmfModel) model).setExpand(true);
+////
+////			// load transformation
+////			IncrementalResource incrementalResource = new PropertyAccessRecordTable();
+////			IncrementalLazyEgxModule module = new IncrementalLazyEgxModule(incrementalResource);
+////			IEolContext context = module.getContext();
+////			URI transformationUri = egxFile.toURI();
+////
+////			module.parse(transformationUri);
+////
+////			// add model to context
+////			context.getModelRepository().addModel(model);
+////
+////			/** execute transformation after model is changed **/
+////			List<LazyGenerationRuleContentPromise> instances = (List<LazyGenerationRuleContentPromise>) module.execute();
+////			((IncrementalLazyEgxModule) module).startRecording();
+////			generateViews(instances, module);
+////			((IncrementalLazyEgxModule) module).stopRecording();
+////
+////			((IncrementalLazyEgxModule) module).getPropertyAccessRecorder().getPropertyAccesses().clear();
+////			
+////			incrementalResource.printIncrementalRecords();
+////
+////			// modify the model
+////			EObject eObject = (EObject) model.getElementById("alice");
+////			EStructuralFeature eNameFeature = eObject.eClass().getEStructuralFeature("name");
+////			System.out.println("Element's name: " + (String) eObject.eGet(eNameFeature));
+////			eObject.eSet(eNameFeature, "Amber");
+////
+////			/** reexecute transformation after model is changed **/
+////			instances = (List<LazyGenerationRuleContentPromise>) module.execute();
+////			((IncrementalLazyEgxModule) module).startRecording();
+////			generateViews(instances, module);
+////			((IncrementalLazyEgxModule) module).stopRecording();
+////
+////			incrementalResource.printIncrementalRecords();
+////
+////			System.out.println("Targets of Modified Properties:" + incrementalResource.getModifiedPropertiesTargets().size());
+////			
+////			for (PropertyAccessRecord property : incrementalResource.getModifiedPropertiesTargets()) {
+////				System.out.println(property);
+////			}
+////			assertEquals(incrementalResource.getModifiedPropertiesTargets().size(), 2);
+////			System.out.println("Finished!");
+////		} catch (Exception e) {
+////			e.printStackTrace();
+////		}
+////	}
+////
+////	private void generateViews(List<LazyGenerationRuleContentPromise> instances, IncrementalLazyEgxModule module) {
+////		for (LazyGenerationRuleContentPromise instance : instances) {
+////			
+////			String format = "svg";
+////			String icon = "";
+////			List<Patch> patches = new ArrayList<>(1);
+////			Collection<String> path = Arrays.asList("");
+////			String pathString = null;
+////			List<Layer> layers = new ArrayList<>();
+////			Variable layersVariable = null;
+////			Integer position = null;
+////
+////			Collection<Variable> instanceVariables = instance.getVariables();
+////
+////			for (Variable variable : instanceVariables) {
+////				Object varValue = variable.getValue();
+////				switch (variable.getName()) {
+////				case "format": {
+////					format = varValue + "";
+////					break;
+////				}
+////				case "path": {
+////					if (!(varValue instanceof Collection)) {
+////						(path = (Collection<String>) (varValue = new ArrayList<>(1))).add(Objects.toString(varValue));
+////					} else if (!((Collection<?>) varValue).isEmpty()) {
+////						path = ((Collection<?>) varValue).stream().map(Objects::toString).collect(Collectors.toList());
+////					}
+////					pathString = "/" + String.join("/", path);
+////					break;
+////				}
+////				case "icon": {
+////					icon = varValue + "";
+////					break;
+////				}
+////				case "position": {
+////					if (varValue instanceof Integer) {
+////						position = (Integer) varValue;
+////					} else if (varValue != null) {
+////						position = Integer.parseInt(varValue.toString());
+////					}
+////					break;
+////				}
+////				case "layers": {
+////					layersVariable = variable;
+////					for (Object layerMapObject : (Iterable<?>) varValue) {
+////						Map<Object, Object> layerMap = (Map<Object, Object>) layerMapObject;
+////						Layer layer = new Layer();
+////						layer.setId(layerMap.get("id") + "");
+////						layer.setTitle(layerMap.get("title") + "");
+////						if (layerMap.containsKey("active")) {
+////							layer.setActive((boolean) layerMap.get("active"));
+////						}
+////						layers.add(layer);
+////					}
+////					break;
+////				}
+////				case "patches": {
+////					if (varValue instanceof List) {
+////						patches = (List<Patch>) varValue;
+////					} else if (varValue instanceof Patch) {
+////						patches.add((Patch) varValue);
+////					} else if (varValue instanceof Collection) {
+////						patches.addAll((Collection<? extends Patch>) varValue);
+////					}
+////					break;
+////				}
+////				}
+////
+////			}
+////
+////			// If this is a custom view there may be an activeLayers variable in the
+////			// variables list
+////			Variable activeLayersVariable = instanceVariables.stream().filter(v -> v.getName().equals("activeLayers"))
+////					.findAny().orElse(null);
+////			if (activeLayersVariable != null) {
+////				Collection<?> activeLayers = (Collection<?>) activeLayersVariable.getValue();
+////				for (Layer layer : layers) {
+////					layer.setActive(activeLayers.contains(layer.getId()));
+////				}
+////			}
+////
+////			// Replace layers variable from list of maps to list of Layer objects
+////			if (layersVariable != null) {
+////				instanceVariables.remove(layersVariable);
+////			}
+////			instanceVariables.add(Variable.createReadOnlyVariable("layers", layers));
+////
+////			ViewTree vt = new ViewTree(instance, format, icon, position, patches, layers);
+////			vt.getContent();
+////			updateIncrementalResource(module, pathString);
+////
+////		}
+//	}
+//	
+//	private void updateIncrementalResource(IncrementalLazyEgxModule module, String pathString) {
+//		for (IPropertyAccess propertyAccess : ((IncrementalLazyEgxModule) module).getPropertyAccessRecorder()
+//				.getPropertyAccesses().all()) {
+//			GenerationRulePropertyAccess generationRulePropertyAccess = (GenerationRulePropertyAccess) propertyAccess;
+//
+//			EObject contextElement = (EObject) generationRulePropertyAccess.getContextElement();
+//			String contextResourceUri = null;
+//			String contextElementId = null;
+//			if (contextElement != null) {
+//				contextResourceUri = ((XMIResource) contextElement.eResource()).getURI().toString();
+//				contextElementId = ((XMIResource) contextElement.eResource()).getID(contextElement);
+//			} else {
+//				System.console();
+//			}
+//
+//			EObject modelElement = (EObject) generationRulePropertyAccess.getModelElement();
+//			String elementResourceUri = ((XMIResource) modelElement.eResource()).getURI().toString();
+//			String elementId = ((XMIResource) modelElement.eResource()).getID(modelElement);
+//
+//			String propertyName = generationRulePropertyAccess.getPropertyName();
+//			Object value = modelElement.eGet(modelElement.eClass().getEStructuralFeature(propertyName));
+//
+//			PropertyAccessRecord record = new PropertyAccessRecord(module.getFile().getAbsolutePath(),
+//					generationRulePropertyAccess.getRule().getName(), contextResourceUri, contextElementId,
+//					elementResourceUri, elementId, propertyName, value, pathString);
+//			System.out.println("added: " + record);
+//			module.getIncrementalResource().add(record);
+//		}
+//		((IncrementalLazyEgxModule) module).getPropertyAccessRecorder().getPropertyAccesses().clear();
+//		System.console();
+//	}
 }
