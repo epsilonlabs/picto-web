e2ef28b38dc9055fa96aa711dadbb087af722439
Alfa Yohannis <ary506@york.ac.uk>
2023-01-11 15:29:06 +0000

    Add PicoCLI

--- org.eclipse.epsilon.picto.web/src/main/java/org/eclipse/epsilon/picto/web/PictoApplication.java
+++ org.eclipse.epsilon.picto.web/src/main/java/org/eclipse/epsilon/picto/web/PictoApplication.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import org.eclipse.epsilon.picto.dom.PictoFactory;
 import org.eclipse.epsilon.picto.dom.PictoPackage;
@@ -24,104 +25,125 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ApplicationContextEvent;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
 /***
  * The main class for Picto Web Application
  * 
  * @author Alfa Yohannis
  *
  */
+
 @SpringBootApplication
-public class PictoApplication implements ApplicationListener<ApplicationContextEvent> {
+public class PictoApplication implements ApplicationListener<ApplicationContextEvent>, Callable<Integer> {
 
-	/***
-	 * Define the relative workspace target
-	 */
-	public static final String WORKSPACE = ".." + File.separator + "workspace" + File.separator;
+  /***
+   * Define the relative workspace target
+   */
+  public static final String WORKSPACE = ".." + File.separator + "workspace" + File.separator;
 
-	/***
-	 * This contains the visualised Picto file's target
-	 */
-	public static final List<File> PICTO_FILES = new ArrayList<File>();
+  /***
+   * This contains the visualised Picto file's target
+   */
+  public static final List<File> PICTO_FILES = new ArrayList<File>();
 
-	/***
-	 * To keep the arguments accessible.
-	 */
-	public static String[] args;
+  /***
+   * To keep the arguments accessible.
+   */
+  public static String[] args;
 
-	private static boolean isViewsGenerationGreedy = false;
+  private static boolean isViewsGenerationGreedy = false;
 
-	private static boolean eachRequestAlwaysRegeneratesView = false;
+  private static boolean eachRequestAlwaysRegeneratesView = false;
 
-	private static List<PictoProject> pictoProjects = new ArrayList<PictoProject>();
+  private static List<PictoProject> pictoProjects = new ArrayList<PictoProject>();
 
-	private static ConfigurableApplicationContext context;
+  private static ConfigurableApplicationContext context;
 
-	private static PictoWebOnLoadedListener pictoWebOnLoadedListener = new PictoWebOnLoadedListener() {
-		@Override
-		public void onLoaded() {
-		}
-	};
+  private static PictoWebOnLoadedListener pictoWebOnLoadedListener = new PictoWebOnLoadedListener() {
+    @Override
+    public void onLoaded() {
+    }
+  };
 
-	private static PromisesGenerationListener promisesGenerationListener = new PromisesGenerationListener() {
-		@Override
-		public void onGenerated(java.util.Set<String> invalidatedViews) {
-		};
-	};
+  private static PromisesGenerationListener promisesGenerationListener = new PromisesGenerationListener() {
+    @Override
+    public void onGenerated(java.util.Set<String> invalidatedViews) {
+    };
+  };
 
-	/***
-	 * Initialise Picto Application
-	 * 
-	 * @param args
-	 * @throws Exception
-	 */
-	public PictoApplication() throws Exception {
-		String workDir = System.getProperty("user.dir");
-		System.out.println("PICTO - Default Picto Application directory: " + workDir);
-		System.out.println("PICTO - Workspace directory: " + (new File(WORKSPACE)).getAbsolutePath());
-	}
+  /***
+   * Initialise Picto Application
+   * 
+   * @param args
+   * @throws Exception
+   */
+  public PictoApplication() throws Exception {
+    String workDir = System.getProperty("user.dir");
+    System.out.println("PICTO - Default Picto Application directory: " + workDir);
+    System.out.println("PICTO - Workspace directory: " + (new File(WORKSPACE)).getAbsolutePath());
+  }
 
-	/***
-	 * Main program launcher
-	 * 
-	 * @param args
-	 * @throws Exception
-	 */
-	public static void main(String[] args) throws Exception {
-		PictoApplication.args = args;
-		context = SpringApplication.run(PictoApplication.class, args);
-		PictoFactory.eINSTANCE.eClass();
-		PictoPackage.eINSTANCE.eClass();
-		FileWatcher.scanPictoFiles();
-		FileWatcher.startWatching();
-		pictoWebOnLoadedListener.onLoaded();
-	}
+  /***
+   * Main program launcher
+   * 
+   * @param args
+   * @throws Exception
+   */
+  @Command(name = "Picto-Web", mixinStandardHelpOptions = true, //
+      version = "0.1.2-alpha", //
+      description = "A High-performance Tool for Complex Model Exploration.")
+  public static void main(String... args) throws Exception {
+    PictoApplication.args = args;
 
-	/**
-	 * @return the pictoProjects
-	 */
-	public static List<PictoProject> getPictoProjects() {
-		return pictoProjects;
-	}
+    commandLine = new CommandLine(new PictoApplication());
+    commandLine.execute(args);
 
-	public static void exit() throws IOException, InterruptedException {
+    context = SpringApplication.run(PictoApplication.class, args);
+    PictoFactory.eINSTANCE.eClass();
+    PictoPackage.eINSTANCE.eClass();
+    FileWatcher.scanPictoFiles();
+    FileWatcher.startWatching();
+    pictoWebOnLoadedListener.onLoaded();
+  }
+
+  public Integer call() throws Exception {
+    return 0;
+  }
+
+  private static CommandLine commandLine;
+
+  @Option(names = { "-ni", "--non-incremental" }, description = "Use no incrementality (false, true).")
+  private static boolean nonIncremental = false;
+
+  /**
+   * @return the pictoProjects
+   */
+  public static List<PictoProject> getPictoProjects() {
+    return pictoProjects;
+  }
+
+  public static void exit() throws IOException, InterruptedException {
 //		PromiseView.getPromiseexecutor().shutdown();
-		FileWatcher.stopWatching();
-		FileViewContentCache.clear();
-		PromiseView.getPromiseExecutor().shutdownNow();
-		SpringApplication.exit(context, new ExitCodeGenerator() {
-			@Override
-			public int getExitCode() {
-				return 0;
-			}
-		});
-	}
+    FileWatcher.stopWatching();
+    FileViewContentCache.clear();
+    PromiseView.getPromiseExecutor().shutdownNow();
+    SpringApplication.exit(context, new ExitCodeGenerator() {
+      @Override
+      public int getExitCode() {
+        return 0;
+      }
+    });
+  }
 
-	public static void setPictoWebOnLoadedListener(PictoWebOnLoadedListener listener) {
-		PictoApplication.pictoWebOnLoadedListener = listener;
-	}
+  public static void setPictoWebOnLoadedListener(PictoWebOnLoadedListener listener) {
+    PictoApplication.pictoWebOnLoadedListener = listener;
+  }
 
-	@Override
-	public void onApplicationEvent(ApplicationContextEvent event) {
+  @Override
+  public void onApplicationEvent(ApplicationContextEvent event) {
 //    if (event instanceof ContextStartedEvent) {
 //      System.out.println("PICTO: started - " + Timestamp.from(Instant.now()).toString());
 //    } else if (event instanceof ContextRefreshedEvent) {
@@ -131,30 +153,30 @@ public class PictoApplication implements ApplicationListener<ApplicationContextE
 //    } else if (event instanceof ContextClosedEvent) {
 //      System.out.println("PICTO: context closed - " + Timestamp.from(Instant.now()).toString());
 //    }
-	}
+  }
 
-	public static boolean isViewsGenerationGreedy() {
-		return isViewsGenerationGreedy;
-	}
+  public static boolean isViewsGenerationGreedy() {
+    return isViewsGenerationGreedy;
+  }
 
-	public static void setViewsGenerationGreedy(boolean viewsGenerationIsGreedy) {
-		PictoApplication.isViewsGenerationGreedy = viewsGenerationIsGreedy;
-	}
+  public static void setViewsGenerationGreedy(boolean viewsGenerationIsGreedy) {
+    PictoApplication.isViewsGenerationGreedy = viewsGenerationIsGreedy;
+  }
 
-	public static boolean getEachRequestAlwaysRegeneratesView() {
-		return eachRequestAlwaysRegeneratesView;
-	}
+  public static boolean getEachRequestAlwaysRegeneratesView() {
+    return eachRequestAlwaysRegeneratesView;
+  }
 
-	public static void setEachRequestAlwaysRegeneratesView(boolean eachRequestAlwaysRegeneratesView) {
-		PictoApplication.eachRequestAlwaysRegeneratesView = eachRequestAlwaysRegeneratesView;
-	}
+  public static void setEachRequestAlwaysRegeneratesView(boolean eachRequestAlwaysRegeneratesView) {
+    PictoApplication.eachRequestAlwaysRegeneratesView = eachRequestAlwaysRegeneratesView;
+  }
 
-	public static PromisesGenerationListener getPromisesGenerationListener() {
-		return promisesGenerationListener;
-	}
+  public static PromisesGenerationListener getPromisesGenerationListener() {
+    return promisesGenerationListener;
+  }
 
-	public static void setPromisesGenerationListener(PromisesGenerationListener promisesGenerationListener) {
-		PictoApplication.promisesGenerationListener = promisesGenerationListener;
-	}
+  public static void setPromisesGenerationListener(PromisesGenerationListener promisesGenerationListener) {
+    PictoApplication.promisesGenerationListener = promisesGenerationListener;
+  }
 
 }
